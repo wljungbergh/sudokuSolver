aba9c3f66459026174a3e91beba1b889cfcee59f
William <william.ljungbergh@hotmail.com>
2019-10-26 18:34:56 +0200

    solve_suduko function working

--- sudokuSolver/sudukosolver.py
+++ sudokuSolver/sudukosolver.py
@@ -1,14 +1,14 @@
 import trainingData
 import numpy as np
 import math
+import cProfile
 
 
 testing = trainingData.TestingCases()
 
-
 def create_array_from_string(string):
     n = int(math.sqrt(len(string)))
-    arr = np.zeros((n,n))
+    arr = np.zeros((n,n), dtype='int')
     for idx, letter in enumerate(string):
         x = idx % n
         y = (idx - x) // n
@@ -16,22 +16,41 @@ def create_array_from_string(string):
             arr[y][x] = int(letter)
     return arr
 
-r = create_array_from_string(testing.expert['start'][0])
+def create_string_from_array(grid):
+    string = ''
+    for y in grid:
+        for x in y:
+            string += str(x)
+
+    return string
 
 def find_empty_cell(grid):
+    n = grid.shape[0]
+    flat = grid.flatten()
+    try:
+        idx = np.where(flat == 0)[0][0]
+        idxx = idx % n
+        idxy =  idx // n
+        return (idxx, idxy)
 
-    for idxy, y in enumerate(grid):
-        for idxx, x in enumerate(y):
-            if x == 0:
-                return (idxx, idxy)
+    except:
+        return False
 
-    return False
+def print_grid(grid):
+    string = create_string_from_array(grid)
+    printed_string = ''
+    for i in range(9):
+        x = i*9
+        printed_string += string[x:x+3] + '|' + string[x+3:x+6] + '|' + string[x+6:x+9] + '\n'
+        printed_string += '----------- \n'
+    
+    print(printed_string)
 
 def valid_for_col(grid, col, number):
-    return number not in [int(t[col]) for t in grid]
+    return number not in grid[:, col]
 
 def valid_for_row(grid, row, number):
-    return number not in [int(t) for t in grid[row]]
+    return number not in grid[row]
 
 def valid_for_box(grid, position, number):
     (x,y) = position
@@ -42,15 +61,57 @@ def valid_for_box(grid, position, number):
     return number not in box
 
 def valid_pos(grid, position, number):
-    (row, col) = position
-    return valid_for_col(grid, col, number) and valid_for_row(grid, row, number) and valid_for_box(grid, position, number)
+    (col, row) = position
+    return valid_for_row(grid, row, number) and valid_for_col(grid, col, number) and valid_for_box(grid, position, number)
 
 def find_next_valid_number(grid, position):
     (x,y) = position
     start_number = grid[y][x]
-    for number in range(start+1,9):
+
+    for number in range(start_number+1,10):
         if valid_pos(grid, position, number):
             return number
     
     return False
+
+def is_solved(grid):
+    return not find_empty_cell(grid)
+
+def solve_suduko(grid):
+    backtrack = False
+    filled_cells = []
+    iterations = 0
+    while not is_solved(grid):
+        iterations += 1
+
+        if not backtrack:
+            pos = find_empty_cell(grid)
+        else:
+            if filled_cells: 
+                pos = filled_cells.pop()
+            else:
+                print("Solve couldn't be found!")
+                return False
+        
+        number = find_next_valid_number(grid, pos)
+        if number:
+            grid[pos[1]][pos[0]] = number
+            filled_cells.append(pos)
+            backtrack = False
+        else:
+            grid[pos[1]][pos[0]] = 0
+            backtrack = True
+    print(f'Number of iterations: {iterations}')
+    return grid
+
+
+if __name__ == '__main__':
+    r = create_array_from_string(testing.expert['start'][0])
+    pr = cProfile.Profile()
+    pr.enable()
+    solved = solve_suduko(r)
+    pr.disable()
+    pr.print_stats()
+    
+   
 
